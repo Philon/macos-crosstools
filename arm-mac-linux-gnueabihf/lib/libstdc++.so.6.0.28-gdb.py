d3f0d7f5d84b38c191ce37da7df32e841959ddd0
Philon <id@philon.cn>
2021-05-23 15:14:04 +0800

    update to 10.3.0

---
+++ arm-mac-linux-gnueabihf/lib/libstdc++.so.6.0.28-gdb.py
@@ -0,0 +1,1 @@
+../../arm-mac-linux-gnueabihf/sysroot/lib/libstdc++.so.6.0.28-gdb.py
---
+++ lib/gcc/arm-mac-linux-gnueabihf/10.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/Volumes/Develop/.build/arm-mac-linux-gnueabihf/src/gcc/configure --build=aarch64-build_apple-darwin20.4.0 --host=aarch64-build_apple-darwin20.4.0 --target=arm-mac-linux-gnueabihf --prefix=/Volumes/Develop/x-tools/arm-mac-linux-gnueabihf --exec_prefix=/Volumes/Develop/x-tools/arm-mac-linux-gnueabihf --with-sysroot=/Volumes/Develop/x-tools/arm-mac-linux-gnueabihf/arm-mac-linux-gnueabihf/sysroot --enable-languages=c,c++ --with-cpu=cortex-a9 --with-fpu=neon-vfpv4 --with-float=hard --with-pkgversion='crosstool-NG 1.24.0.349_6d00833' --enable-__cxa_atexit --disable-libmudflap --disable-libgomp --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-libsanitizer --disable-libmpx --with-gmp=/Volumes/Develop/.build/arm-mac-linux-gnueabihf/buildtools --with-mpfr=/Volumes/Develop/.build/arm-mac-linux-gnueabihf/buildtools --with-mpc=/Volumes/Develop/.build/arm-mac-linux-gnueabihf/buildtools --with-isl=/Volumes/Develop/.build/arm-mac-linux-gnueabihf/buildtools --enable-lto --enable-threads=posix --enable-target-optspace --enable-plugin --enable-gold --disable-nls --disable-multilib --with-local-prefix=/Volumes/Develop/x-tools/arm-mac-linux-gnueabihf/arm-mac-linux-gnueabihf/sysroot --enable-long-long";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "cortex-a9" }, { "float", "hard" }, { "fpu", "neon-vfpv4" }, { "tls", "gnu" } };
